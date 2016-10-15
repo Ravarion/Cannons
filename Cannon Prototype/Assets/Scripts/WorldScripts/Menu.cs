753a472ce48c5a1e8f3b04a8a48e52b85859dcaf
terryc0158 <terryc0158@my.uwstout.edu>
2016-10-15 13:11:13 -0500

    Pause Menu + PowerUpPrefabs
    Completed pause menu and some power up prefabs.

---
+++ Cannon Prototype/Assets/Scripts/WorldScripts/Menu.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject mainMenu;
+    public bool paused;
+
+    void Start()
+    {
+        paused = false;
+    }
+	void Update ()
+    {
+        //pause
+        if (Input.GetKeyDown(KeyCode.T) && paused == false)
+        {
+            Time.timeScale = 0;
+            mainMenu.SetActive(true);
+            //if the current cannon is the main cannon then effect that one
+            foreach (GameObject mainCannon in GameObject.FindGameObjectsWithTag("MainCannon"))
+            {
+
+                if (mainCannon.GetComponent<PlayerController>().currentCannon == true)
+                {
+                    mainCannon.GetComponent<MouseLook>().enabled = false;
+                    mainCannon.GetComponent<PlayerController>().enabled = false;
+                }
+            }
+            //if the current cannon is a subcannon cannon then effect that one
+            foreach (GameObject cannon in GameObject.FindGameObjectsWithTag("Cannon"))
+            {
+
+                if (cannon.GetComponent<PlayerController>().currentCannon == true)
+                {
+                    cannon.GetComponent<MouseLook>().enabled = false;
+                    cannon.GetComponent<PlayerController>().enabled = false;
+                }
+            }
+            paused = true;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        //unpause
+        else if (Input.GetKeyDown(KeyCode.T) && paused == true)
+        {
+            Time.timeScale = 1;
+            mainMenu.SetActive(false);
+            foreach (GameObject mainCannon in GameObject.FindGameObjectsWithTag("MainCannon"))
+            {
+
+                if (mainCannon.GetComponent<PlayerController>().currentCannon == true)
+                {
+                    mainCannon.GetComponent<MouseLook>().enabled = true;
+                    mainCannon.GetComponent<PlayerController>().enabled = true;
+                }
+            }
+            foreach (GameObject cannon in GameObject.FindGameObjectsWithTag("Cannon"))
+            {
+
+                if (cannon.GetComponent<PlayerController>().currentCannon == true)
+                {
+                    cannon.GetComponent<MouseLook>().enabled = true;
+                    cannon.GetComponent<PlayerController>().enabled = true;
+                }
+            }
+            paused = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+    public void ResumeButton()
+    {
+        Time.timeScale = 1;
+        mainMenu.SetActive(false);
+        foreach (GameObject mainCannon in GameObject.FindGameObjectsWithTag("MainCannon"))
+        {
+            if (mainCannon.GetComponent<PlayerController>().currentCannon == true)
+            {
+                mainCannon.GetComponent<MouseLook>().enabled = true;
+                mainCannon.GetComponent<PlayerController>().enabled = true;
+            }
+        }
+        foreach (GameObject cannon in GameObject.FindGameObjectsWithTag("Cannon"))
+        {
+
+            if (cannon.GetComponent<PlayerController>().currentCannon == true)
+            {
+                cannon.GetComponent<MouseLook>().enabled = true;
+                cannon.GetComponent<PlayerController>().enabled = true;
+            }
+        }        
+        paused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
