c057c966be3a2be38489161511178508f72db020
Beltshazzar <beltshazzar@live.com>
2016-10-18 19:29:03 -0500

    Glider now lets you change flight path and breaks on collision

--- Cannon Prototype/Assets/Scripts/CannonAttributes/Glider.cs
+++ Cannon Prototype/Assets/Scripts/CannonAttributes/Glider.cs
@@ -4,13 +4,18 @@ using System.Collections;
 public class Glider : CannonAttribute {
 
     public float liftForce = 0.5f;
-    private bool glideBreak = false;
+    public bool glideBreak = false;
 
     //Provides constant upward Force to slow desent
     void FixedUpdate()
     {
+
+        //print(GetComponent<Rigidbody>().velocity.ToString());
+        //print(transform.forward.ToString());
+
         if(glideBreak == false)
         {
+            GliderYaw(GetComponent<Rigidbody>());
             GetComponent<Rigidbody>().AddForce(Vector3.up * liftForce);
         }
     }
@@ -19,7 +24,20 @@ public class Glider : CannonAttribute {
     //NOT IMPLEMENTED!
     void OnCollisionEnter(Collision col)
     {
+        if(col.gameObject.tag != "Pellet")
+        {
+            glideBreak = true;
+        }
+        
 //        gameObject.AddComponent<CannonAttribute>();
 //        Destroy(this);
+    }
+
+    void GliderYaw(Rigidbody rig)
+    {
+        float mag = Mathf.Sqrt(rig.velocity.x * rig.velocity.x + rig.velocity.z * rig.velocity.z);
+        Vector3 norm = new Vector3(transform.forward.x, 0, transform.forward.z).normalized;
+
+        rig.velocity = new Vector3(mag * norm.x, rig.velocity.y, mag * norm.z);
     }
 }
